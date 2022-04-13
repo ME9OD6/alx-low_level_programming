144090b71c51079a4ab608a88bf1bb5e24206ae6
ME9OD6 <melodymburu04@gmail.com>
2022-04-13 03:54:36 -0700

    Show alphabet or return 0

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * _isalpha - checks for alphabetic character
+ * @c: character to check case
+ * Return: Always 0 (Success)
+ */
+int _isalpha(int c)
+{
+	return (c >= 33 && c <= 126);
+}
