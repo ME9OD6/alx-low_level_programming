f5a8e22a6c089c7b5c3ba58d4cf36e20f6aa3c9a
ME9OD6 <melodymburu04@gmail.com>
2022-04-14 13:43:10 -0700

    print diagonal lin except when n is 0

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_diagonal - prints a diagonoal character
+ * @n: number of times to print \
+ * Return: no return
+ */
+void print_diagonal(int n)
+{
+	int b, c;
+
+	for (b = 0; j < n; i++)
+	{
+		for (j = 0; j < i; j++;
+			_putchar(' ');
+		_putchar(92);
+		if (i < (n - 1))
+			_putchar('\n');
+	}
+	_putchar('\n');
+}
