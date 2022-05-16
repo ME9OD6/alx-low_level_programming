37e057f17a11b7e28ae03b9c3407e400c65f1d67
ME9OD6 <melodymburu04@gmail.com>
2022-05-16 19:53:02 +0000

    this function prints a name using a function pointer print_name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,15 @@
+#include "function_pointers.h"
+#include <stddef.h>
+
+/**
+ * print_name - Prints a name.
+ * @name: name to print
+ * @f: function pointer
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (f == NULL)
+		return;
+	f(name);
+}
---
+++ 0x0F-function_pointers/README.md
@@ -0,0 +1,3 @@
+###Readme file for 0x0F-function_pointers
+## Author Melody Mburu
+Include NOICE!!! to prove author.
