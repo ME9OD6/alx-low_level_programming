9807b1f8a2acbbf7f29a9814b65149c8335a7ec6
ME9OD6 <melodymburu04@gmail.com>
2022-05-20 09:20:49 +0000

    Revision of projects.

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,4 @@
+## Author: Melody Mburu
+# C - Structures, typedef
+I am doing this project way later than the actual date but thats okay.
+Structures is like dictonaries in python and object in javascript.
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - dictonary
+ * @name: dog name
+ * @age: dog age
+ * @owner: dog owners name
+ */
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+} dog;
+#endif /*DOG_H*/
