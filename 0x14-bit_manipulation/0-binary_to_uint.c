eecf53817085b2b788061cd6f96252ef650d637f
ME9OD6 <melodymburu04@gmail.com>
2022-05-31 12:34:56 +0300

    readme, main and first file

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * binary_to_uint - convert binary number to insigned int
+ * @b: char string
+ *
+ * Return: converted decimal number or 0
+ */
+unsigned int binary_to_uint(const char *b)
+{
+    unsigned int sum, power;
+    int length;
+
+    if (b == NULL)
+        return (0);
+
+    for (length = 0; b[length]; length++)
+    {
+        if (b[length] != '0' && b[length] != '1')
+            return (0);
+    }
+    for (power = 1, sum = 0, length--; length >= 0; length--, power *= 2)
+    {
+        if (b[length] == '1')
+            sum += power;
+    }
+    return (sum);
+}
---
+++ 0x14-bit_manipulation/README.md
@@ -0,0 +1,9 @@
+## Melody Mburu
+# Bit Manipulation
+Learn about:
+1. &(and)
+2. |(or)
+3. ~(compliment)
+4. ^(xor)
+5. <<(left shift which means variable x 2^right operator)
+6. >>(right shift which meand variable / 2^right operator)
