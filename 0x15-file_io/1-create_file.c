0acd6907c5e29c59788a881474416ce35199db9b
ME9OD6 <melodymburu04@gmail.com>
2022-06-03 00:51:40 +0300

    ---

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -25,27 +25,19 @@ int get_length(char *str)
 
 int create_file(const char *filename, char *text_content)
 {
-	int fd;
-	unsigned int length;
-	ssize_t bytes_written;
+	char *str;
+	int fd, wr, len = 0;
 
-	if (filename == NULL)
+	if (!filename)
 		return (-1);
+	str = (!text_content) ? "" : text_content;
+	len = get_length(str);
 	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
 	if (fd == -1)
 		return (-1);
-
-	if (text_content == NULL)
-	{
-		length = 0;
-		text_content = "";
-	}
-	else
-		length = _strlen(text_content);
-
-	bytes_written = write(fd, text_content, length);
-	if (bytes_written == -1)
+	wr = write(fd, str, len);
+	if (wr == -1)
 		return (-1);
 	close(fd);
-        return (1);
+	return (1);
 }
