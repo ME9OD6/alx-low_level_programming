95ef45c9a6ef3b82ac4dd87eef711c334d314b6d
ME9OD6 <melodymburu04@gmail.com>
2022-06-03 01:09:07 +0300

    --

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,43 +1,54 @@
 #include "main.h"
 
 /**
- * get_length - returns the number of characters in a string
- * @str: string to count
- *
+ * _strlen - returns length of string
+ * @s: string
  * Return: length of string
  */
-int get_length(char *str)
-{
-	int i, length = 0;
 
-	for (i = 0; str[i] != '\0'; i++)
-		length++;
-	return (length);
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (s[len] != '\0')
+	{
+		len++;
+	}
+
+	return (len);
 }
+
 
 /**
  * create_file - creates a file
  * @filename: name of file
  * @text_content: text
- * 
  * Return: 1 - success, -1 error
  */
 
 int create_file(const char *filename, char *text_content)
 {
-	char *str;
-	int fd, wr, len = 0;
+	int fd;
+	unsigned int length;
+	ssize_t bytes_written;
 
-	if (!filename)
+	if (filename == NULL)
 		return (-1);
-	str = (!text_content) ? "" : text_content;
-	len = get_length(str);
-	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 	if (fd == -1)
 		return (-1);
-	wr = write(fd, str, len);
-	if (wr == -1)
+
+	if (text_content == NULL)
+	{
+		length = 0;
+		text_content = "";
+	}
+	else
+		length = _strlen(text_content);
+
+	bytes_written = write(fd, text_content, length);
+	if (bytes_written == -1)
 		return (-1);
 	close(fd);
 	return (1);
-}
+}S
