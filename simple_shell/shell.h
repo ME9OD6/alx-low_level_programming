79d36d1c80a1a2b895470c3dde5737e7787062fd
ME9OD6 <melodymburu04@gmail.com>
2022-06-10 06:53:09 +0300

    main, readme

---
+++ simple_shell/shell.h
@@ -0,0 +1,21 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+/* header files */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <errno.h>
+
+/* Global variable */
+extern char **environ;
+
+
+
+
+#endif 
