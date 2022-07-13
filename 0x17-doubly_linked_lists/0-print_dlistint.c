a72a54810e0c37dae30560fca9d86b6cf33913eb
ME9OD6 <melodymburu04@gmail.com>
2022-07-13 10:39:24 +0000

    dynamic library liball.so

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -7,14 +7,12 @@
  */
 size_t print_dlistint(const dlistint_t *h)
 {
-        size_t nodes = 0;
-
-        while (h)
+	size_t nodes = 0;
+	while (h)
         {
-                nodes++;
+		nodes++;
                 printf("%d\n", h->n);
                 h = h->next;
         }
-
         return (nodes);
 }
